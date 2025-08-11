2503cc918e63e9af13d7607cb361d16e27205199
shakeel4-debug <shakeel.softwarequality@gmail.com>
2025-08-11 17:20:12 +0500

    Completed prime number

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,2 +1,18 @@
 public class PrimeNumber {
+    public static void main(String[] args) {
+        int number=11;
+        boolean isPrime=false;
+        for(int i=2;i<number;i++){
+            if (number%i==0){
+                isPrime=true;
+                break;
+            }
+        }
+        if (isPrime==true){
+            System.out.println("It is not prime");
+        }
+        else {
+            System.out.println("It is prime");
+        }
+    }
 }
